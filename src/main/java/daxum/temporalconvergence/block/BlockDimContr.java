02ec4e022807237689f36ce87c47b279435e71bc
daxum <daxum144@gmail.com>
2017-04-07 13:43:26 -0500

    Initial implementation of controller

--- src/main/java/daxum/temporalconvergence/block/BlockDimContr.java
+++ src/main/java/daxum/temporalconvergence/block/BlockDimContr.java
@@ -1,10 +1,59 @@
 package daxum.temporalconvergence.block;
 
-//WIP
-public class BlockDimContr extends BlockBase {
+import daxum.temporalconvergence.item.ModItems;
+import daxum.temporalconvergence.tileentity.TileDimContr;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockDimContr extends BlockBase implements ITileEntityProvider {
 
 	public BlockDimContr() {
 		super("dim_controller", 5.0f, 30.0f, "pickaxe", 2);
 	}
 
+	@Override
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new TileDimContr();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float x, float y, float z) {
+		ItemStack stack = player.getHeldItem(hand);
+
+		if (stack.getItem() == ModItems.dimLinker && stack.hasTagCompound() && stack.getTagCompound().hasKey("dimid")) {
+			TileEntity te = world.getTileEntity(pos);
+
+			if (te != null && te instanceof TileDimContr) {
+				((TileDimContr) te).setId(stack.getTagCompound().getInteger("dimid"));
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public void breakBlock(World world, BlockPos pos, IBlockState state) {
+		TileEntity te = world.getTileEntity(pos);
+
+		if (te != null && te instanceof TileDimContr)
+			((TileDimContr) te).unFreezeDim();
+	}
+
+	@Override
+	public void neighborChanged(IBlockState state, World world, BlockPos pos, Block changed, BlockPos changedPos) {
+		if (world.getTileEntity(pos) instanceof TileDimContr) {
+			if (world.getStrongPower(pos) > 0)
+				((TileDimContr)world.getTileEntity(pos)).freezeDim();
+			else
+				((TileDimContr)world.getTileEntity(pos)).unFreezeDim();
+		}
+	}
 }
---
+++ src/main/java/daxum/temporalconvergence/tileentity/TileDimContr.java
@@ -0,0 +1,93 @@
+package daxum.temporalconvergence.tileentity;
+
+import daxum.temporalconvergence.power.PowerDimension;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileDimContr extends TileEntity {
+	protected int linkId = -1;
+	protected boolean didThisFreeze = false;
+
+	public void freezeDim() {
+		if (world.isRemote || didThisFreeze) return;
+
+		PowerDimension connected = PowerDimension.get(world, linkId);
+
+		if (connected != null) {
+			connected.addFreezer();
+			didThisFreeze = true;
+			world.notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 8);
+		}
+	}
+
+	public void unFreezeDim() {
+		if (!world.isRemote) {
+			PowerDimension connected = PowerDimension.get(world, linkId);
+
+			if (connected != null && didThisFreeze) {
+				connected.removeFreezer();
+				didThisFreeze = false;
+				world.notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 8);
+			}
+		}
+	}
+
+	public void setId(int i) {
+		if (!world.isRemote && i != linkId && i >= 0) {
+			if (didThisFreeze) {
+				PowerDimension old = PowerDimension.get(world, linkId);
+
+				if (old != null)
+					old.removeFreezer();
+
+				linkId = i;
+
+				PowerDimension current = PowerDimension.get(world, linkId);
+
+				if (current != null)
+					current.addFreezer();
+			}
+			else {
+				linkId = i;
+			}
+
+			world.notifyBlockUpdate(pos, world.getBlockState(pos), world.getBlockState(pos), 8);
+		}
+	}
+
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound comp) {
+		if (linkId >= 0) //Negative id's are invalid
+			comp.setInteger("linkid", linkId);
+		comp.setBoolean("freeze", didThisFreeze);
+
+		return super.writeToNBT(comp);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound comp) {
+		if (comp.hasKey("linkid"))
+			linkId = comp.getInteger("linkId");
+		if (comp.hasKey("freeze"))
+			didThisFreeze = comp.getBoolean("freeze");
+
+		super.readFromNBT(comp);
+	}
+
+	@Override
+	public SPacketUpdateTileEntity getUpdatePacket() {
+		return new SPacketUpdateTileEntity(pos, -42, writeToNBT(new NBTTagCompound()));
+	}
+
+	@Override
+	public NBTTagCompound getUpdateTag() {
+		return writeToNBT(new NBTTagCompound());
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+		readFromNBT(pkt.getNbtCompound());
+	}
+}
