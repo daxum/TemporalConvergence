0d4f71241382090d49bfe7e606bd50903c11c136
daxum <daxum144@gmail.com>
2017-04-10 14:45:04 -0500

    Rendering for dimContr

--- src/main/java/daxum/temporalconvergence/render/tileentity/TileDimContrRenderer.java
+++ src/main/java/daxum/temporalconvergence/render/tileentity/TileDimContrRenderer.java
@@ -13,6 +13,7 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
 
+//TODO: This should probably be made into some sort of model
 public class TileDimContrRenderer extends TileEntitySpecialRenderer<TileDimContr> {
 	public static final ResourceLocation SOLID_BLACK = new ResourceLocation(TemporalConvergence.MODID + ":textures/test_texture.png");
 	public static final float RADIUS = 0.25f;
@@ -20,88 +21,109 @@ public class TileDimContrRenderer extends TileEntitySpecialRenderer<TileDimContr
 	protected static boolean compiled = false;
 	protected static int displayList;
 
-
 	@Override
 	public void renderTileEntityAt(TileDimContr te, double transformX, double transformY, double transformZ, float partialTicks, int destroyStage) {
+		if (te.renderScale <= 0 && te.getId() == -1) return;
+
 		GlStateManager.pushMatrix();
 		GlStateManager.translate(transformX + 0.5, transformY + 0.5, transformZ + 0.5);
+		GlStateManager.rotate(te.getWorld().getTotalWorldTime() + partialTicks, 0.0f, 1.0f, 0.0f);
 		GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
 		GlStateManager.disableLighting();
 
 		bindTexture(SOLID_BLACK);
 
-		callSphereDisplayList();
+		if (te.renderScale >= 1.0f && te.getId() != -1) {
+			callSphereDisplayLists();
+		}
+		else if (te.renderScale <= 1.0f && te.getId() != -1){
+			renderSpheresDirect(RADIUS * te.renderScale);
+			te.renderScale += 0.05;
+		}
+		else if (te.renderScale > 0 && te.getId() == -1) {
+			renderSpheresDirect(RADIUS * te.renderScale);
+			te.renderScale -= 0.05;
+		}
 
 		GlStateManager.enableLighting();
 		GlStateManager.popMatrix();
 	}
 
-	public void callSphereDisplayList() {
+	public void callSphereDisplayLists() {
 		if (!compiled)
-			compileSphereDisplayList();
+			compileSphereDisplayLists();
 		GlStateManager.callList(displayList);
 	}
 
-	private void compileSphereDisplayList() {
+	public void renderSpheresDirect(float radius) {
+		renderSphere(radius, 5, 0.0f, 0.5f, 0.0f, 1.0f, false);
+		renderSphere(radius * 1.05f, 15, 0.5f, 0.5f, 0.0f, 1.0f, true);
+	}
+
+	private void compileSphereDisplayLists() {
 		displayList = GLAllocation.generateDisplayLists(1);
 		GlStateManager.glNewList(displayList, GL11.GL_COMPILE);
 
-		renderSphere(RADIUS);
+		renderSphere(RADIUS, 5, 0.0f, 0.5f, 0.0f, 1.0f, false);
+		renderSphere(RADIUS * 1.05f, 15, 0.5f, 0.5f, 0.0f, 1.0f, true);
 
 		GlStateManager.glEndList();
+
 		compiled = true;
 	}
 
-	private void renderSphere(float radius) {
+	private void renderSphere(float radius, int step, float texStartX, float texWidthX, float texStartY, float texWidthY, boolean mesh) {
 		Tessellator tess = Tessellator.getInstance();
 		VertexBuffer vb = tess.getBuffer();
 
-		vb.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_TEX);
+		int glMode = mesh ? GL11.GL_LINE_STRIP : GL11.GL_TRIANGLE_STRIP;
 
 		//The sphere is complete.
 		boolean alt = true;
-		for (int i = 0; i < 360; i += 5) {
+		for (int i = 0; i < 360; i += step) {
+			vb.begin(glMode, DefaultVertexFormats.POSITION_TEX);
 			if (!alt) {
-				for (int j = 0; j <= 185; j += 5) {
+				for (int j = 0; j <= 180 + step; j += step) {
 					if (alt) {
 						float theta = j * PIF / 180.0f;
 						float phi = i * PIF / 180.0f;
 						float sintheta = MathHelper.sin(theta);
 
-						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta)).tex(j / 185.0, j / 185.0).endVertex();
+						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta));
 					}
 					else {
 						float theta = j * PIF / 180.0f;
-						float phi = (i + 5) * PIF / 180.0f;
+						float phi = (i + step) * PIF / 180.0f;
 						float sintheta = MathHelper.sin(theta);
 
-						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta)).tex(j / 185.0, j / 185.0).endVertex();
+						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta));
 					}
+					vb.tex(j / (180.0 + step) * texWidthX + texStartX, i / 359.0 * texWidthY + texStartY).endVertex();
 					alt = !alt;
 				}
 			}
 			else {
-				for (int j = 180; j >= -5; j -= 5) {
+				for (int j = 180; j >= -step; j -= step) {
 					if (alt) {
 						float theta = j * PIF / 180.0f;
 						float phi = i * PIF / 180.0f;
 						float sintheta = MathHelper.sin(theta);
 
-						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta)).tex(j / 185.0, j / 185.0).endVertex();
+						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta));
 					}
 					else {
 						float theta = j * PIF / 180.0f;
-						float phi = (i + 5) * PIF / 180.0f;
+						float phi = (i + step) * PIF / 180.0f;
 						float sintheta = MathHelper.sin(theta);
 
-						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta)).tex(j / 185.0, j / 185.0).endVertex();
+						vb.pos(radius * sintheta * MathHelper.cos(phi), radius * sintheta * MathHelper.sin(phi), radius * MathHelper.cos(theta));
 					}
+					vb.tex((j + step) / (180.0 + step) * texWidthX + texStartX, i / 359.0 * texWidthY + texStartY).endVertex();
 					alt = !alt;
 				}
 			}
 			alt = !alt;
+			tess.draw();
 		}
-
-		tess.draw();
 	}
 }
