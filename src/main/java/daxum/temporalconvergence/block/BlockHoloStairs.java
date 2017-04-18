88a5f855d827cfbcdf54e5085c72b6be9fd2a965
daxum <daxum144@gmail.com>
2017-04-17 23:16:17 -0500

    Partially made stairs for the buildings

---
+++ src/main/java/daxum/temporalconvergence/block/BlockHoloStairs.java
@@ -0,0 +1,52 @@
+/***************************************************************************
+ * Temporal Convergence
+ * Copyright (C) 2017
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ **************************************************************************/
+package daxum.temporalconvergence.block;
+
+import java.util.List;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BlockHoloStairs extends BlockBase {
+
+	public BlockHoloStairs() {
+		super("holo_stairs");
+	}
+
+	@Override
+	public void addCollisionBoxToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entity, boolean actualState) {
+		if (entity != null && entity.posY > pos.getY() + 0.9 && (!(entity instanceof EntityPlayer) || !entity.isSneaking()))
+			super.addCollisionBoxToList(state, world, pos, entityBox, collidingBoxes, entity, actualState);
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+}
