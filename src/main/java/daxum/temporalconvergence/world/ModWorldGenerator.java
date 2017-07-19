cc35ccd0a8295ef1759e289c92bb66f74e66b6e3
daxum <daxum144@gmail.com>
2017-07-19 16:44:18 -0500

    Fixed loading new chunks during world generation

--- src/main/java/daxum/temporalconvergence/world/ModWorldGenerator.java
+++ src/main/java/daxum/temporalconvergence/world/ModWorldGenerator.java
@@ -22,6 +22,7 @@ package daxum.temporalconvergence.world;
 import java.util.Random;
 
 import daxum.temporalconvergence.TemporalConvergence;
+import daxum.temporalconvergence.block.BlockTimePlant;
 import daxum.temporalconvergence.block.ModBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.EnumFacing;
@@ -35,8 +36,9 @@ import net.minecraftforge.fml.common.IWorldGenerator;
 public class ModWorldGenerator implements IWorldGenerator {
 	@Override
 	public void generate(Random rand, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
-		if (world.provider.getDimension() == 0)
+		if (world.provider.getDimension() == 0) {
 			generateTimePlants(world, rand, chunkX, chunkZ, 3, 55, 80);
+		}
 	}
 
 	public void generateTimePlants(World world, Random rand, int chunkX, int chunkZ, int chancesToSpawn, int minHeight, int maxHeight) {
@@ -48,16 +50,16 @@ public class ModWorldGenerator implements IWorldGenerator {
 		int heightDiff = Math.abs(maxHeight - minHeight) + 1;
 
 		for (int i = 0; i < chancesToSpawn; i++) {
-			int x = chunkX * 16 + rand.nextInt(16);
+			int x = chunkX * 16 + rand.nextInt(14) + 1;
 			int y = minHeight + rand.nextInt(heightDiff);
-			int z = chunkZ * 16 + rand.nextInt(16);
+			int z = chunkZ * 16 + rand.nextInt(14) + 1;
 
 			BlockPos pos = new BlockPos(x, y, z);
 			BlockPos downPos = pos.down();
 			IBlockState state = world.getBlockState(downPos);
 
 			if (world.isAirBlock(pos) && state.getBlock().canSustainPlant(state, world, downPos, EnumFacing.UP, (IPlantable) ModBlocks.TIME_PLANT)) {
-				world.setBlockState(pos, ModBlocks.TIME_PLANT.getDefaultState(), 2);
+				world.setBlockState(pos, ModBlocks.TIME_PLANT.getDefaultState().withProperty(BlockTimePlant.WITHERED, false), 2);
 			}
 		}
 	}
