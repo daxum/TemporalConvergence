c4e64bdd1e72b023948d77738fb186192d7643a4
daxum <daxum144@gmail.com>
2017-11-11 19:19:37 -0600

    Syncing for crafter

--- src/main/java/daxum/temporalconvergence/tileentity/TileCrafter.java
+++ src/main/java/daxum/temporalconvergence/tileentity/TileCrafter.java
@@ -24,16 +24,19 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import daxum.temporalconvergence.TemporalConvergence;
 import daxum.temporalconvergence.power.DirectPowerHelper;
 import daxum.temporalconvergence.power.IDirectPowerProvider;
 import daxum.temporalconvergence.power.IDirectPowerReceiver;
 import daxum.temporalconvergence.power.PowerRequirements;
 import daxum.temporalconvergence.power.PowerType;
+import daxum.temporalconvergence.power.PowerTypeList;
+import daxum.temporalconvergence.power.PowerTypeManager;
 import daxum.temporalconvergence.recipes.CrafterRecipes;
 import daxum.temporalconvergence.util.WorldHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
@@ -53,8 +56,7 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 
 	@Override
 	public void update() {
-		if (crafting) {
-			TemporalConvergence.LOGGER.info("Crafting!");
+		if (!world.isRemote && crafting) {
 			if (craftTicks > 0) {
 				craftTicks--;
 			}
@@ -79,7 +81,6 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 				}
 
 				int got = DirectPowerHelper.getPowerDistributed(providerList, type, amount);
-				TemporalConvergence.LOGGER.info("Got {} power of type {}", got, type);
 
 				recipePower.get(i).add(got);
 
@@ -100,6 +101,8 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 
 				stopCrafting();
 			}
+
+			sendBlockUpdate();
 		}
 	}
 
@@ -108,14 +111,10 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 			return false;
 		}
 
-		TemporalConvergence.LOGGER.info("Attempting start");
-
 		currentRecipe = getRecipeList();
 
 		ItemStack centerItem = currentRecipe.get(CrafterInventory.CENTER_ITEM_INDEX);
 		List<ItemStack> items = currentRecipe.subList(1, currentRecipe.size());
-
-		TemporalConvergence.LOGGER.info("Center: {}, List: {}", centerItem, items);
 
 		if (CrafterRecipes.isValidRecipe(centerItem, items)) {
 			PowerRequirements requirements = CrafterRecipes.getPower(centerItem, items);
@@ -133,6 +132,7 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 			}
 
 			crafting = true;
+			sendBlockUpdate();
 
 			return true;
 		}
@@ -142,6 +142,7 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 
 	public void stopCrafting() {
 		crafting = false;
+		sendBlockUpdate();
 	}
 
 	private List<ItemStack> getRecipeList() {
@@ -238,6 +239,46 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 			gotten = 0;
 		}
 
+		public PowerData(NBTTagCompound comp) {
+			boolean missing = false;
+
+			if (comp.hasKey("type", Constants.NBT.TAG_STRING)) {
+				type = PowerTypeManager.getType(comp.getString("type"));
+			}
+			else {
+				missing = true;
+				type = PowerTypeList.POWER_0;
+			}
+
+			if (comp.hasKey("req", Constants.NBT.TAG_INT)) {
+				required = comp.getInteger("req");
+			}
+			else {
+				missing = true;
+				required = 0;
+			}
+
+			if (comp.hasKey("per", Constants.NBT.TAG_INT)) {
+				perTick = comp.getInteger("per");
+			}
+			else {
+				missing = true;
+				perTick = 0;
+			}
+
+			if (comp.hasKey("got", Constants.NBT.TAG_INT)) {
+				gotten = comp.getInteger("got");
+			}
+			else {
+				missing = true;
+				gotten = 0;
+			}
+
+			if (missing) {
+				throw new IllegalArgumentException("Invalid tag to PowerData constructor");
+			}
+		}
+
 		public void add(int amount) {
 			gotten += amount;
 		}
@@ -245,22 +286,102 @@ public class TileCrafter extends TileEntityBase implements TileEntityInventoried
 		public boolean done() {
 			return gotten >= required;
 		}
+
+		public NBTTagCompound serializeNBT() {
+			NBTTagCompound comp = new NBTTagCompound();
+
+			comp.setString("type", type.getName());
+			comp.setInteger("req", required);
+			comp.setInteger("per", perTick);
+			comp.setInteger("got", gotten);
+
+			return comp;
+		}
 	}
+
+	private static final String INVENTORY_TAG = "inv";
+	private static final String CRAFT_TAG = "craft";
+	private static final String CRAFT_TICK_TAG = "ctick";
+	private static final String CURRENT_RECIPE_TAG = "recipe";
+	private static final String POWER_TAG = "power";
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound comp) {
-		comp.setTag("inv", inventory.serializeNBT());
+		comp.setTag(INVENTORY_TAG, inventory.serializeNBT());
+		comp.setBoolean(CRAFT_TAG, crafting);
+		comp.setInteger(CRAFT_TICK_TAG, craftTicks);
+		saveRecipe(comp);
+		savePower(comp);
 
 		return super.writeToNBT(comp);
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound comp) {
-		if (comp.hasKey("inv", Constants.NBT.TAG_COMPOUND)) {
-			inventory.deserializeNBT(comp.getCompoundTag("inv"));
+		if (comp.hasKey(INVENTORY_TAG, Constants.NBT.TAG_COMPOUND)) {
+			inventory.deserializeNBT(comp.getCompoundTag(INVENTORY_TAG));
 		}
 
+		if (comp.hasKey(CRAFT_TAG, Constants.NBT.TAG_BYTE)) {
+			crafting = comp.getBoolean(CRAFT_TAG);
+		}
+
+		if (comp.hasKey(CRAFT_TICK_TAG, Constants.NBT.TAG_INT)) {
+			craftTicks = comp.getInteger(CRAFT_TICK_TAG);
+		}
+
+		loadRecipe(comp);
+		loadPower(comp);
+
 		super.readFromNBT(comp);
+	}
+
+	private void saveRecipe(NBTTagCompound comp) {
+		NBTTagList list = new NBTTagList();
+
+		for (int i = 0; i < currentRecipe.size(); i++) {
+			list.appendTag(currentRecipe.get(i).serializeNBT());
+		}
+
+		comp.setTag(CURRENT_RECIPE_TAG, list);
+	}
+
+	private void loadRecipe(NBTTagCompound comp) {
+		if (!comp.hasKey(CURRENT_RECIPE_TAG, Constants.NBT.TAG_LIST)) {
+			return;
+		}
+
+		NBTTagList list = comp.getTagList(CURRENT_RECIPE_TAG, Constants.NBT.TAG_COMPOUND);
+
+		currentRecipe.clear();
+
+		for (NBTBase argh : list) {
+			currentRecipe.add(new ItemStack((NBTTagCompound)argh));
+		}
+	}
+
+	private void savePower(NBTTagCompound comp) {
+		NBTTagList list = new NBTTagList();
+
+		for (int i = 0; i < recipePower.size(); i++) {
+			list.appendTag(recipePower.get(i).serializeNBT());
+		}
+
+		comp.setTag(POWER_TAG, list);
+	}
+
+	private void loadPower(NBTTagCompound comp) {
+		if (!comp.hasKey(POWER_TAG, Constants.NBT.TAG_LIST)) {
+			return;
+		}
+
+		NBTTagList list = comp.getTagList(POWER_TAG, Constants.NBT.TAG_COMPOUND);
+
+		recipePower.clear();
+
+		for (NBTBase argh : list) {
+			recipePower.add(new PowerData((NBTTagCompound)argh));
+		}
 	}
 
 	@Override
